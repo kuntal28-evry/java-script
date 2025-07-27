35122ffe0070d0247b44f43f733952f6bc21ad38
kuntal28-evry <mandalkuntal28@gmail.com>
2025-07-27 14:08:12 +0000

    01_variables

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "mandal@28.com"
+var accountPassword ="1234"
+accountCity = "jaipur"
+
+
+//accountId = 2 //not allowed
+
+accountEmail ="hc@hc.com"
+accountPassword="2121"
+accountCity="Bangaluru"
+
+/*
+prefer not use var because 
+of issue block socpe and functional scope 
+
+*/
+
+console.log(accountId);
+console.table([accountId.accountCity,accountEmail,accountPassword])
